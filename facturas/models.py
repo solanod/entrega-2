d802c75bc4fd2a50d481cd23a16069b2f7fe06d6
solanod <116456032+solanod@users.noreply.github.com>
2023-05-18 20:42:49 -0500

    para presentar

--- facturas/models.py
+++ facturas/models.py
@@ -1,16 +1,17 @@
 from django.db import models
+from django.core.exceptions import ValidationError
 
 
 class Detalle_Factura(models.Model):
     cod_fac = models.IntegerField(primary_key=True)
-    detalle_producto = models.CharField(max_length=42, blank=True, null=True)
-    cantidad = models.IntegerField(blank=True)
-    precio_unitario = models.IntegerField(blank=True)
-    precio_total = models.IntegerField(blank=True)
-    cliente = models.CharField (max_length=50, blank=True)
-    direccion_envio = models.CharField (max_length=150, blank=True)
-    telefono = models.CharField (max_length=50)  
-    fecha_compra = models.DateTimeField(blank=True)
+    detalle_producto = models.CharField(max_length=42, null=True)
+    cantidad = models.IntegerField(null=True)
+    precio_unitario = models.IntegerField(null=True)
+    precio_total = models.IntegerField(null=True)
+    cliente = models.CharField (max_length=50, null=True)
+    direccion_envio = models.CharField (max_length=150, null=True)
+    telefono = models.CharField (max_length=50,null=True)  
+    fecha_compra = models.DateTimeField(null=True)
     def __str__(self):
         return'{}'.format(self.cod_fac)
 
